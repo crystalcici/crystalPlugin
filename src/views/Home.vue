655f70aa09fcdb76daf93e8fa829fa6474ed4751
crystalcici <517455797@qq.com>
2021-07-25 22:44:57 +0800

    update: 搭建官网html结构

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="top-nav">
+      <div class="logo"></div>
+      <div class="menu"></div>
+    </div>
+    <div class="banner">
+      <h1>crystalPlugin-vue3</h1>
+      <h2>学习vue3用</h2>
+      <p class="actions">
+        <a>Github</a>
+        <a>开始</a>
+      </p>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
