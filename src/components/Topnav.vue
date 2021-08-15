8238f2f61793510088ecb8d89a0cb3b6c38adc77
crystalcici <517455797@qq.com>
2021-08-15 22:17:00 +0800

    feat: 完善aside切换逻辑

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="top-nav">
-    <div class="logo" @click="toogleMenu">LOGO</div>
+    <div class="logo">LOGO</div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
-    <span class="toggleAside"></span>
+    <span class="toogleAside" @click="toogleMenu"></span>
   </div>
 </template>
 
@@ -43,6 +43,16 @@ export default {
     li{
       margin: 0 1em
     }
+  }
+  .toogleAside{
+    display: none;
+    width: 24px;
+    height: 24px;
+    background: red;
+    position: absolute;
+    left: 16px;
+    top: 50%;
+    transform: translateY(-50%);
   }  
   @media (max-width: 500px) {
     .menu{
@@ -50,6 +60,9 @@ export default {
     }
     .logo{
       margin: 0 auto
+    }
+    .toogleAside{
+      display: inline-block;
     }
   } 
 }
